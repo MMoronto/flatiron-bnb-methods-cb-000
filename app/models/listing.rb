a57386ff7c2427ac39662e7d6b70869a0ebaa538
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-06-24 19:40:34 +0000

    Automatically backed up by Learn

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,7 +5,6 @@ class Listing < ActiveRecord::Base
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
   
-  
   validates :address, presence: true
   validates :listing_type, presence: true
   validates :title, presence: true
